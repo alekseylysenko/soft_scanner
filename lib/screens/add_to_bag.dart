d758ecbbe8e777417a0a0db19c1f16dbccfb147e
alekseylysenko <lysenko-a@mail.ru>
2021-09-13 08:14:21 +0300

    корректировка размеров шрифта

--- lib/screens/add_to_bag.dart
+++ lib/screens/add_to_bag.dart
@@ -6,12 +6,20 @@ import 'package:soft_scanner/widget/boxes.dart';
 
 class AddToBagScreen extends StatefulWidget {
   const AddToBagScreen({Key? key}) : super(key: key);
-
+  
   @override
   _AddToBagScreenState createState() => _AddToBagScreenState();
 }
 
 class _AddToBagScreenState extends State<AddToBagScreen> {
+
+   void deleteEquipment(Equipment equipment) {
+    // final box = Boxes.getTransactions();
+    // box.delete(transaction.key);
+
+ equipment.delete();
+    //setState(() => transactions.remove(transaction));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,10 +35,18 @@ class _AddToBagScreenState extends State<AddToBagScreen> {
                 itemCount: equipment.length,
                 itemBuilder: (context, index) {
                   final equipmentSingle = equipment[index];
-                  return ListTile(
-                    title: Text(equipmentSingle.name),
-                    subtitle: Text(equipmentSingle.code),
-                    leading: Text(equipmentSingle.dateReceiving!.day.toString() + "-" + equipmentSingle.dateReceiving!.month.toString() + "-" + equipmentSingle.dateReceiving!.year.toString()),
+                  return Container(
+                    padding: EdgeInsets.only(right: 10, left: 10, top: 5),
+                    child: Card(
+                      child: ListTile(
+                        title: (Text(equipmentSingle.name + "\n" + equipmentSingle.dateReceiving!.day.toString() + "-" + equipmentSingle.dateReceiving!.month.toString() + "-" + equipmentSingle.dateReceiving!.year.toString(), style: TextStyle(fontSize: 14),) ),               
+                        leading:  Text(equipmentSingle.code, style: TextStyle(fontSize: 10)),
+                        trailing:  TextButton.icon(
+                            label: Text('Удалить'),
+                            icon: Icon(Icons.delete, size: 24,),
+                            onPressed: () => deleteEquipment(equipmentSingle),)
+                      ),
+                    ),
                   );
                 },
               );
