a0d9e3793d84787ffdbea34e228d6c6b30d630f7
alekseylysenko <lysenko-a@mail.ru>
2021-09-21 00:01:46 +0300

    изменение в рюкзаке и истории

--- lib/screens/bag_screen.dart
+++ lib/screens/bag_screen.dart
@@ -15,7 +15,7 @@ class BagScreen extends StatefulWidget {
 class _BagScreenState extends State<BagScreen> {
   final numberController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  
+
   void editEquipment(Equipment equipment, int applicationNumber) {
     // Рекдактирование Оборудования
 
@@ -28,8 +28,8 @@ class _BagScreenState extends State<BagScreen> {
 
     equipment.save();
   }
-  @override
 
+  @override
   void _showSimpleDialog(equipmentSingle) {
     showDialog<void>(
       context: context,
@@ -66,10 +66,10 @@ class _BagScreenState extends State<BagScreen> {
                   if (_formKey.currentState!.validate()) {
                     editEquipment(
                         equipmentSingle, int.parse(numberController.text));
-                    
+
                     final snackBar = SnackBar(
                       content: Text('Добавлено в историю'),
-                      action: SnackBarAction(   
+                      action: SnackBarAction(
                         label: 'X',
                         onPressed: () {},
                       ),
@@ -89,106 +89,122 @@ class _BagScreenState extends State<BagScreen> {
     return Scaffold(
         appBar: AppBar(
           title: Text("Мой рюкзак"),
-
         ),
         body: ValueListenableBuilder<Box<Equipment>>(
           valueListenable: Boxes.getEquipment().listenable(),
           builder: (context, box, _) {
-            final equipment = box.values.toList().cast<Equipment>();
+            //final equipment = box.values.toList().cast<Equipment>();
 
-            return ListView.builder(
+            final List<Equipment> equipment = box.values
+                .where((element) => element.deleteEquipment == false)
+                .toList();
+            return equipment.length > 0
+            ? ListView.builder(
                 itemCount: equipment.length,
                 itemBuilder: (context, index) {
                   final equipmentSingle = equipment[index];
-                  if (equipmentSingle.deleteEquipment == false) {
-                    return Container(
-                      padding: EdgeInsets.only(right: 10, left: 10, top: 5),
-                      child: Card(
-                        color: Colors.grey[100],
-                          child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                  flex: 5,
-                                  child: Container(
-                                      padding: EdgeInsets.only(
-                                          left: 10, top: 5, bottom: 5),
-                                      child: Text("S/N:"))),
-                              Expanded(
-                                  flex: 5,
-                                  child: Container(
-                                      padding: EdgeInsets.only(
-                                          left: 10, top: 5, bottom: 5),
-                                      child:
-                                          SelectableText(equipmentSingle.code)))
-                            ],
-                          ),
-                          SizedBox(
-                            height: 7,
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                  flex: 5,
-                                  child: Container(
-                                      padding:
-                                          EdgeInsets.only(left: 10, bottom: 5),
-                                      child: Text('Наименование:'))),
-                              Expanded(
-                                  flex: 5,
-                                  child: Container(
-                                      padding:
-                                          EdgeInsets.only(left: 10, bottom: 5),
-                                      child: Text(equipmentSingle.name)))
-                            ],
-                          ),
-                          SizedBox(
-                            height: 7,
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                  flex: 5,
-                                  child: Container(
-                                      padding:
-                                          EdgeInsets.only(left: 10, bottom: 5),
-                                      child: Text('Дата получения:'))),
-                              Expanded(
-                                  flex: 5,
-                                  child: Container(
-                                      padding:
-                                          EdgeInsets.only(left: 10, bottom: 5),
-                                      child: Text(DateFormat("dd.MM.yyyy")
-                                          .format(
-                                              equipmentSingle.dateReceiving!))))
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                flex: 6,
-                                child: Container(),
-                              ),
-                              Expanded(
-                                flex: 4,
-                                child: Container(
-                                  child: TextButton(
-                                    child: Text("Установить"),
-                                    onPressed: () {
-                                      _showSimpleDialog(equipmentSingle);
-                                    },
+                
+                      return Container(
+                          padding: EdgeInsets.only(right: 10, left: 10, top: 5),
+                          child: Card(
+                              color: Colors.grey[100],
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, top: 5, bottom: 5),
+                                              child: Text("S/N:"))),
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, top: 5, bottom: 5),
+                                              child: SelectableText(
+                                                  equipmentSingle.code)))
+                                    ],
                                   ),
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      )),
-                    );
-                  }
-                  return Container();
-                });
+                                  SizedBox(
+                                    height: 7,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child: Text('Наименование:'))),
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child:
+                                                  Text(equipmentSingle.name)))
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 7,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child: Text('Дата получения:'))),
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child: Text(
+                                                  DateFormat("dd.MM.yyyy")
+                                                      .format(equipmentSingle
+                                                          .dateReceiving!))))
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        flex: 6,
+                                        child: Container(),
+                                      ),
+                                      Expanded(
+                                        flex: 4,
+                                        child: Container(
+                                          child: TextButton(
+                                            child: Text("Установить"),
+                                            onPressed: () {
+                                              _showSimpleDialog(
+                                                  equipmentSingle);
+                                            },
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              )),
+                        );
+                      
+                })
+                : Center(
+                          child:
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                              Center(child: Icon(Icons.library_books, color: Colors.grey,size: 56,),),
+                              SizedBox(height: 16,),
+                              Text('В рюкзаке пусто', style: TextStyle(color: Colors.grey, fontSize: 28),)
+                            ],)
+                         );
+                      
           },
         ));
   }
--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -22,153 +22,163 @@ class _HistoryScreenState extends State<HistoryScreen> {
     equipment.delete();
     //setState(() => transactions.remove(transaction));
   }
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("История перемещений"),
         ),
-        body: 
-        
-         ValueListenableBuilder<Box<Equipment>>(
+        body: ValueListenableBuilder<Box<Equipment>>(
             valueListenable: Boxes.getEquipment().listenable(),
             builder: (context, box, _) {
-              final equipment = box.values.toList().cast<Equipment>();           
-              return ListView.builder(
-                  itemCount: equipment.length,
-                  itemBuilder: (context, index) {
-                    final equipmentSingle = equipment[index];
+              // final equipment = box.values.toList().cast<Equipment>();
+              final List<Equipment> equipment = box.values
+                  .where((element) => element.deleteEquipment == true)
+                  .toList();
+              return equipment.length > 0
+                  ? ListView.builder(
+                      itemCount: equipment.length,
+                      itemBuilder: (context, index) {
+                        final equipmentSingle = equipment[index];
 
-                    return equipmentSingle.deleteEquipment == true
-                        ? Container(
-                            padding:
-                                EdgeInsets.only(right: 10, left: 10, top: 5),
-                            child: Card(
-                                color: Colors.grey[100],
-                                child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Expanded(
-                                        flex: 5,
-                                        child: Container(
-                                          padding: EdgeInsets.only(
-                                              left: 10, top: 5, bottom: 5),
-                                          child: Text("№ заявки"),
-                                        )),
-                                    Expanded(
-                                        flex: 5,
-                                        child: Container(
+                        return Container(
+                          padding: EdgeInsets.only(right: 10, left: 10, top: 5),
+                          child: Card(
+                              color: Colors.grey[100],
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
                                             padding: EdgeInsets.only(
                                                 left: 10, top: 5, bottom: 5),
-                                            child: SelectableText(
-                                                equipmentSingle
-                                                    .applicationNumber
-                                                    .toString())))
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                        flex: 5,
+                                            child: Text("№ заявки"),
+                                          )),
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, top: 5, bottom: 5),
+                                              child: SelectableText(
+                                                  equipmentSingle
+                                                      .applicationNumber
+                                                      .toString())))
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, top: 5, bottom: 5),
+                                              child: Text("S/N:"))),
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, top: 5, bottom: 5),
+                                              child: SelectableText(
+                                                  equipmentSingle.code)))
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 7,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child: Text('Наименование:'))),
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child:
+                                                  Text(equipmentSingle.name)))
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 7,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child: Text('Дата получения:'))),
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child: Text(
+                                                  DateFormat("H:m dd.MM.yyyy")
+                                                      .format(equipmentSingle
+                                                          .dateReceiving!)))),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child: Text('Дата установки:'))),
+                                      Expanded(
+                                          flex: 5,
+                                          child: Container(
+                                              padding: EdgeInsets.only(
+                                                  left: 10, bottom: 5),
+                                              child: Text(DateFormat(
+                                                      "H:m dd.MM.yyyy")
+                                                  .format(equipmentSingle
+                                                      .dateInstallation!)))),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        flex: 6,
+                                        child: Container(),
+                                      ),
+                                      Expanded(
+                                        flex: 4,
                                         child: Container(
-                                            padding: EdgeInsets.only(
-                                                left: 10, top: 5, bottom: 5),
-                                            child: Text("S/N:"))),
-                                    Expanded(
-                                        flex: 5,
-                                        child: Container(
-                                            padding: EdgeInsets.only(
-                                                left: 10, top: 5, bottom: 5),
-                                            child: SelectableText(
-                                                equipmentSingle.code)))
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 7,
-                                ),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                        flex: 5,
-                                        child: Container(
-                                            padding: EdgeInsets.only(
-                                                left: 10, bottom: 5),
-                                            child: Text('Наименование:'))),
-                                    Expanded(
-                                        flex: 5,
-                                        child: Container(
-                                            padding: EdgeInsets.only(
-                                                left: 10, bottom: 5),
-                                            child: Text(equipmentSingle.name)))
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 7,
-                                ),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                        flex: 5,
-                                        child: Container(
-                                            padding: EdgeInsets.only(
-                                                left: 10, bottom: 5),
-                                            child: Text('Дата получения:'))),
-                                    Expanded(
-                                        flex: 5,
-                                        child: Container(
-                                            padding: EdgeInsets.only(
-                                                left: 10, bottom: 5),
-                                            child: Text(
-                                                DateFormat("H:m dd.MM.yyyy")
-                                                    .format(equipmentSingle
-                                                        .dateReceiving!)))),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                        flex: 5,
-                                        child: Container(
-                                            padding: EdgeInsets.only(
-                                                left: 10, bottom: 5),
-                                            child: Text('Дата установки:'))),
-                                    Expanded(
-                                        flex: 5,
-                                        child: Container(
-                                            padding: EdgeInsets.only(
-                                                left: 10, bottom: 5),
-                                            child: Text(
-                                                DateFormat("H:m dd.MM.yyyy")
-                                                    .format(equipmentSingle
-                                                        .dateInstallation!)))),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      flex: 6,
-                                      child: Container(),
-                                    ),
-                                    Expanded(
-                                      flex: 4,
-                                      child: Container(
-                                        child: TextButton(
-                                          child: Text("Удалить"),
-                                          onPressed: () {
-                                            deleteEquipment(equipmentSingle);
-                                          },
+                                          child: TextButton(
+                                            child: Text("Удалить"),
+                                            onPressed: () {
+                                              deleteEquipment(equipmentSingle);
+                                            },
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            )),
-                          )
-                        : Container();
-                  });
+                                    ],
+                                  )
+                                ],
+                              )),
+                        );
+                      })
+                  : Center(
+                          child:
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                              Center(child: Icon(Icons.library_books, color: Colors.grey,size: 56,),),
+                              SizedBox(height: 16,),
+                              Text('Нет перемещений', style: TextStyle(color: Colors.grey, fontSize: 26),)
+                            ],)
+                         );
             }));
   }
 }
